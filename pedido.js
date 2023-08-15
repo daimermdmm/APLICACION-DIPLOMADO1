08e9ad06abeb9fd1ff2eacd5c52ddb35a9d3316b
daimermdmm <142065318+daimermdmm@users.noreply.github.com>
2023-08-14 21:54:47 -0500

    Update pedido.js
    colocamos las variables que se deben repetir en caso de que el cliente quiera agregar otro producto.

--- pedido.js
+++ pedido.js
@@ -1,5 +1,11 @@
 const Pedido=("pedido");
 var nombreCliente=prompt("Cliente");
 var NitCliente=prompt("Nit");
-
+do{
+    let item=prompt("item")
+    let producto=prompt("producto")    
+    let precio=prompt("precio");
+    let cantidad=prompt("cantidad");
+}
+while(confirm("desea agregar otro producto"));
 

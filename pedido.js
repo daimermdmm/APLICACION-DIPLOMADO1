e9525ad32bc56cfa657e348714b54e4faf1678b6
daimermdmm <142065318+daimermdmm@users.noreply.github.com>
2023-08-14 21:57:21 -0500

    Update pedido.js
    agrgamos las operaciones para los calculos del subtota, iva y total. vemos que es necesario agregar un contador.

--- pedido.js
+++ pedido.js
@@ -1,11 +1,16 @@
 const Pedido=("pedido");
 var nombreCliente=prompt("Cliente");
 var NitCliente=prompt("Nit");
+var subtotal=0
 do{
     let item=prompt("item")
     let producto=prompt("producto")    
     let precio=prompt("precio");
     let cantidad=prompt("cantidad");
+    let subtotal1= precio*cantidad;
+subtotal=subtotal+subtotal1;
+var iva=0.19*subtotal;
+var total=subtotal+iva;
 }
 while(confirm("desea agregar otro producto"));
 
